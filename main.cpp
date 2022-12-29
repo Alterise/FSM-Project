d4c963b39cc4534f8d77f2957491dea97be431fa
Alex <alterise.job@gmail.com>
2022-12-30 00:09:41 +0300

    Add draw() method to Playground class

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <tinyfsm.hpp>
 #include <chrono>
 #include <thread>
+#include <memory>
 
 using namespace sf;
 
-void move(RenderWindow& window, Shape& object, const sf::Vector2<float>& shift,
+Color backgroundColor = Color(150, 250, 150);
+
+// TODO: добавить отрисовку внешних фигур
+void move(const std::shared_ptr<RenderWindow>& window, Shape& object, const sf::Vector2<float>& shift,
           const std::chrono::milliseconds& duration = std::chrono::milliseconds(500)) {
     auto startTime = std::chrono::steady_clock::now();
     auto framesCount = duration / std::chrono::milliseconds(1000 / 60);
@@ -13,9 +17,9 @@ void move(RenderWindow& window, Shape& object, const sf::Vector2<float>& shift,
     auto frameShift = shift / static_cast<float>(framesCount);
     for (int i = 0; i < framesCount; ++i) {
         object.setPosition(object.getPosition() + frameShift);
-        window.clear(Color(120, 200, 30, 0));
-        window.draw(object);
-        window.display();
+        window->clear(backgroundColor);
+        window->draw(object);
+        window->display();
         std::this_thread::sleep_for(frameDuration);
     }
 }
@@ -28,61 +32,71 @@ enum class Object {
 };
 
 class Playground {
-public:
+private:
     int m_Padding;
     int m_Height;
     int m_Width;
-private:
+    std::shared_ptr<RenderWindow> m_window;
+    RectangleShape shape;
 
+    std::vector<std::vector<Object>> m_Data;
 public:
-    Playground() {
+    explicit Playground(std::shared_ptr<RenderWindow>& window) {
+        m_window = window;
         m_Padding = 50;
         m_Height = 10;
         m_Width = 10;
         m_Data = std::vector<std::vector<Object>>(m_Height, std::vector<Object>(m_Width, Object::NOTHING));
+        Vector2f windowSize = static_cast<const Vector2f>(m_window->getSize());
+        shape = RectangleShape(static_cast<const Vector2f>(windowSize - Vector2f(m_Padding * 2, m_Padding * 2)));
+        shape.setFillColor(Color(240,230,140));
+        shape.setPosition(Vector2f(m_Padding, m_Padding));
     }
 
-    std::vector<std::vector<Object>> m_Data;
+    void draw() {
+        m_window->draw(shape);
+    }
 };
 
 int main()
 {
-    RenderWindow window(VideoMode(800, 800), "FSM Coursework");
+    std::shared_ptr<RenderWindow> window(new RenderWindow(VideoMode(800, 800), "FSM Coursework"));
     CircleShape circle(50.f);
-
-    while (window.isOpen())
+    auto pg = Playground(window);
+    while (window->isOpen())
     {
         auto startTime = std::chrono::steady_clock::now();
         Event event{};
-        while (window.pollEvent(event))
+        while (window->pollEvent(event))
         {
             if (event.type == Event::Closed) {
-                window.close();
+                window->close();
             }
         }
 
-        window.clear(Color(120, 200, 30, 0));
+        window->clear(backgroundColor);
 
-//        if (event.type == Event::TextEntered) {
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-//                move(window, circle, Vector2<float>{0, -50});
-//            }
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-//                move(window, circle, Vector2<float>{-50, 0});
-//            }
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-//                move(window, circle, Vector2<float>{0, 50});
-//            }
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-//                move(window, circle, Vector2<float>{50, 0});
-//            }
-//        }
+        if (event.type == Event::TextEntered) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                move(window, circle, Vector2<float>{0, -50});
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                move(window, circle, Vector2<float>{-50, 0});
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                move(window, circle, Vector2<float>{0, 50});
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                move(window, circle, Vector2<float>{50, 0});
+            }
+        }
 
         circle.setFillColor(Color(200, 0, 0));
 
-        window.draw(circle);
+        pg.draw();
+        window->draw(circle);
 
-        window.display();
+        window->display();
     }
 
     return 0;
